82764e96a584810f4d424ebe9f1faa6fc3d60a95
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-03-11 09:47:40 +0700

    update code

--- kpl_jurnalmodul5_2311104058/Program.cs
+++ kpl_jurnalmodul5_2311104058/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 public class Penjumlahan
 {
@@ -9,6 +10,49 @@ public class Penjumlahan
         temp += angka2;
         temp += angka3;
         return temp;
+    }
+}
+
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    // Method untuk menambahkan data baru dan waktu input dari user
+    public void AddNewDataFromUser()
+    {
+        Console.Write("Masukkan data baru: ");
+        string inputData = Console.ReadLine();
+
+        Console.Write("Masukkan waktu input (format: yyyy-MM-dd HH:mm:ss): ");
+        string inputDate = Console.ReadLine();
+
+        try
+        {
+            T data = (T)Convert.ChangeType(inputData, typeof(T));
+            DateTime date = DateTime.ParseExact(inputDate, "yyyy-MM-dd HH:mm:ss", null);
+            storedData.Add(data);
+            inputDates.Add(date);
+            Console.WriteLine("Data dan waktu berhasil ditambahkan!\n");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Input tidak valid: {e.Message}\n");
+        }
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
+        }
     }
 }
 
@@ -27,7 +71,13 @@ public class Program
 
             Penjumlahan penjumlahan = new Penjumlahan();
             int hasil = penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
-            Console.WriteLine($"Hasil penjumlahan: {hasil}");
+            Console.WriteLine($"Hasil penjumlahan: {hasil}\n");
+
+            SimpleDataBase<int> database = new SimpleDataBase<int>();
+            database.AddNewDataFromUser();
+            database.AddNewDataFromUser();
+            database.AddNewDataFromUser();
+            database.PrintAllData();
         }
         else
         {
@@ -35,3 +85,4 @@ public class Program
         }
     }
 }
+//Dimastian Aji Wibowo
